f02a5877461fa58582cd86f60648bd66ecd36e4f
Iliya_Komkov <komckoovik@yandex.ru>
2026-03-24 23:09:36 +0300

    lesson 1

--- leson_1/leson1_1.py
+++ leson_1/leson1_1.py
@@ -1,1 +1,30 @@
-print("привет мир")
+# a = 7
+# b = 8.34
+# str_1 = "Легко в IT"
+# # b = a
+# # a = 8
+# # print(b)
+#
+# # a = b = c = 0
+# # a, b, c = 0, 0, 0
+#
+# # a, b = 8, 10
+# # a, b = b, a
+# # print(a)
+# # print(b)
+#
+# print(type(a))
+# print(type(b))
+# print(type(str_1))
+
+# message = "привет,Даниил"
+# count = 0
+# Count = a
+
+a = c =  4
+b, d = "Какая нибудь строка" , "Какая нибудь  строка"
+
+print(id(a))
+print(id(c))
+print(id(b))
+print(id(d))
