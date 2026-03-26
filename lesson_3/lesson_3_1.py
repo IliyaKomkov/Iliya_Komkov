b5dda39fc77469a3d509c926a8c3c3f8e5cf25b4
Iliya_Komkov <komckoovik@yandex.ru>
2026-03-26 22:08:00 +0300

    lesson 3

---
+++ lesson_3/lesson_3_1.py
@@ -0,0 +1,25 @@
+# a = "string"
+# print(34, a)
+# print(34, 432, "qwerty" a)
+
+# print(2 + 2 * 2)
+
+# a = 12
+# b = 3
+# c = 89
+
+# print(a, b, c, sep=" ", end="\n")
+# print(a, b, c, sep=" | ")
+# print("результат умножения числа", a, "и  числа", b, "равен", a * b )
+# print(f"результат умножения числа {a}  и числа {b} равен {a * b}")# f - string
+
+# a = float(input())
+# print(type(a))
+# print(a)
+
+# a = float(input("введите длинну прямоугольника: ",))
+# b = float(input("введите ширину прямоугольника: ",))
+#
+# print(f"Периметр: {2 * (a + b)}")
+
+
