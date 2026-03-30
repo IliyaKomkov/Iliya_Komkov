d232885eae1c4b161e2deaef3a8344bf2464d00b
Iliya_Komkov <komckoovik@yandex.ru>
2026-03-30 16:47:15 +0300

    homework 1

---
+++ lesson_2/homework_L2.py
@@ -0,0 +1,119 @@
+#1 Задание
+# a = 8
+# b = 3
+# x = 2.5
+# y = -1.7
+# print(a, b, x, y)
+# print(type(a))
+# print(type(b))
+# print(type(x))
+# print(type(y))
+
+#2 ЗАДАНИЕ
+# num1 = 9
+# num2 = 4
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num1 % num2)
+# print(num1 ** num2)
+
+#3 ЗАДАНИЕ
+# a = 10
+# b = 3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# a = -10
+# b = 3
+# a1 = 10
+# b1 = -3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+#
+# print(a1 / b1)
+# print(a1 // b1)
+# print(a1 % b1)
+
+#4 ЗАДАНИЕ
+
+# a = 5
+# b = 3
+# c = 2
+# d = 10
+#
+# print((a + b) * c)
+# print(a + b * c)
+# print((d / c) ** 2)
+
+# #5 ЗАДАНИЕ
+# count = 10
+# print(count + 5)
+# print(count - 3)
+# print(count * 2)
+# print(count / 4)
+# print((count + 5) + (count - 3) + (count * 2) + (count / 4))
+
+#СТРОКИ
+#1 задание
+
+# s1 = "python"
+# s2 = "Программирование"
+# print(s1, s2)
+# s2 = """
+# python Программирование
+# """
+# print(s2)
+
+# empty = ""
+# print(len(empty))
+
+#2 задание
+
+# first_name = "Иван"
+# last_name = "Петров"
+# full_name = (first_name + " " + last_name)
+# print(full_name)
+
+# s = "возраст"
+# age = 25
+# print(s + " " +  str(age))
+
+# a = "xa"
+# print(a * 5)
+# print(a * 2,5) # умножать на вещественное число вррде нельзя
+
+# text = "Привет Мир"
+# print(len(text))
+# len(text)
+# text2 = ""
+# len(text2)
+
+# sentence = "Я изучаю Python"
+# print("Python" in sentence)
+# print("java" in sentence) # Пришлось посмотреть видео, необходимо закрепить
+
+# a = "apple"
+# b = "banana"
+# print( a == b)
+# print( a != b)
+# print( a < b)
+# print( a > b)
+# print( a <= b)
+# print( a >= b)# Надо заучить получше
+
+# a = "А"
+# b = "а"
+# c = "Я"
+# print(ord(a))
+# print(ord(b))
+# print(ord(c))
+
+
+
+
+
