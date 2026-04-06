2cb5ba12c59f62e5d7fec852f7d9ead9161cf4a8
Iliya_Komkov <komckoovik@yandex.ru>
2026-04-06 14:18:21 +0300

    homework 1

---
+++ lesson_3/home_work_2.py
@@ -0,0 +1,92 @@
+# print("Привет, Мир")
+# print(5, 10, 15)
+# print(10 + 25)
+# & = (1, 2, 3)
+# print(# не получилось
+
+# s1 = "Python"
+# s2 = "лучший язык"
+# print(s1)
+# print(s2)
+# print(s1, s2, end= "")
+
+# x = 3.14
+# y = -8
+# print(f"Координаты точки: {x, y}")
+
+# name = "Иван"
+# age = "25"
+# print(f"Имя пользователя: {name}, возраст: {age})")
+
+# name = (input("Привет: Илья "))
+# print(name)
+
+# не совсем понятно задание в преобразованием типов
+
+# a = 5 > 3
+# b = 10 < 2
+# c = 7 == 7
+# a1 = 6 != 8
+# b1 = 4 >= 4
+# c1 = 9 <= 3
+# print(a)
+# print(b)
+# print(c)
+# print(a1)
+# print(b1)
+# print(c1)
+
+# res = 8 > 12
+# print(type(res))
+
+# x = 15
+# print(x % 2 == 0)
+# print(x % 5 == 0)
+# print(x % 3 == 0 or x % 5 == 0)
+
+# y = 4.5
+# print(y >= 1 and y <= 10)
+# print(y >= 0 and y <= 5 or y >= 10 and y <= 15)
+# print(not(y < 5))
+
+# print("True or False and False")
+# print("not False and True")
+# print("False or not True and True")
+# print(not (10 > 5 or 3 < 1))
+# не понял задание
+
+# print(bool(0))
+# print(bool(-5))
+# print(bool(3.14))
+# print(bool(""))
+# print(bool("Python"))
+# print(bool(" "))
+
+# n = 12
+# print(n >= 0 or n % 2 == 0 or n / 3)
+
+# s = "Программирование"
+# print(s[1])
+# print(s[-1])
+# print(s[3], s[-2])
+
+# print(s[100]) - будет ошибка
+# print(len(s[:-1]))
+# print(s[:6])
+# print(s[3: 7])
+# print(s[::2])
+# print(s[:: - 1])
+# print(s[::3])
+# print(s[::-2])
+# print(s[0])# ошибки нет, вылез первый символ
+# s2 = "П"
+# print(s[1] + s2[::2] забыл как делать
+
+# wrd = "abcdefgh"
+# print(wrd[2:5])
+# print(wrd[::-1])
+# print(wrd[2:-2])
+
+
+
+
